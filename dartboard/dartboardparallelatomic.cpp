749f73e7f1c2e4ea33e6d3a60bb54b8eb8cf38b8
Jhonny Alejandro Arango <arango.johnny.alejandro@gmail.com>
2019-06-20 07:28:30 -0500

    Add files via upload

---
+++ dartboard/dartboardparallelatomic.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
+
+
+
+
+
+double calcPiDartboardAtomic(long n){
+        long hits=0, k, chunk=n/8;
+        const double factor = 1.0 / RAND_MAX;
+        double pi_approx, x, y;
+        int threadmasterId = 0, numThreads = 0;
+        #pragma omp parallel private(x,y,k,threadmasterId,numThreads) shared(hits,chunk) //num_threads(8)
+        {
+            threadmasterId = omp_get_thread_num();
+            if(threadmasterId == 0){
+                numThreads = omp_get_num_threads();
+                // printf("Thread master! with id %d\n",threadmasterId);
+                // printf("Invoked threads number %d",numThreads);
+            }
+                for(k=0; k<n; ++k){
+                    /* find two numbers within 0..1*/
+                    x=rand()*factor;
+                    y=rand()*factor;
+                    if(x*x + y*y < 1.0){
+                        #pragma omp atomic
+                        ++hits;
+                    }
+                }
+
+
+                }
+                pi_approx= 4.0*hits/n;
+
+            printf("Approximation of pi after %ld tosses %.20f %.9f %% \n", n, pi_approx, fabs(M_PI-pi_approx)*100/M_PI);
+}
+void writeTime(float elapsed, long len){
+    FILE *f =fopen("timesdartboardparallelatomic.txt","a+");
+    fprintf(f, "%ld  %.9f\n", len, elapsed);
+    fclose(f);
+}
+
+int main (void){
+    long n;
+    while(1){
+    cout<<"enter the no of tosses (or 0 to exit):";
+    cin>>n;
+    srand((int)clock());
+     if(n<=0)
+        break;
+    auto startTime=chrono::high_resolution_clock::now();
+    calcPiDartboardAtomic(n);
+    auto endTime=chrono::high_resolution_clock::now();
+	chrono::duration<float>  elapsed = endTime - startTime;
+	writeTime(elapsed.count(), n);
+    }
+    return 0;
+}
+
---
+++ dartboard/dartboardsecuential.cpp
@@ -0,0 +1,59 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+            double calcPiDartboard(long n){
+                long k,hits;
+                const double factor=1.0/RAND_MAX;
+
+                for (k=hits=0;k<n; k++){
+                    double x=rand() * factor;
+                    double y=rand() * factor;
+                    if(x*x + y*y < 1.0)
+                        ++hits;
+
+                }
+                double pi_approx=4.0* hits /n;
+                cout << "Approximation of pi after"
+                     << n <<"tosses: " << pi_approx
+                     <<" (error="
+                     << fabs(M_PI - pi_approx)*100/M_PI
+                     <<"%)\n";
+            }
+
+void writeTime(float elapsed, long len){
+    FILE *f =fopen("timesdartboard.txt","a+");
+    fprintf(f, "%ld  %.9f\n", len, elapsed);
+    fclose(f);
+}
+
+
+int main(void){
+    long n;
+    while(1){
+    printf("enter the no of tosses (or 0 to exit):");
+    scanf("%f",&n);
+    srand((int)clock());
+     if(n<=0)
+        break;
+    auto startTime=chrono::high_resolution_clock::now();
+    calcPiDartboard(n);
+    auto endTime=chrono::high_resolution_clock::now();
+	chrono::duration<float>  elapsed = endTime - startTime;
+	writeTime(elapsed.count(), n);
+    }
+
+    return 0;
+}
